693570758bea9c0007f15307f0fd07f9e6385128
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2019-12-25 20:31:27 -0300

    Deleting Department view and controller

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWebMVC.Models;
-using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
